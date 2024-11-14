6fedeb07e83a2df210688dd8e931b93d0b217255
dmquiroga11 <qui24003@byui.edu>
2024-11-14 13:23:28 -0500

    New commit

--- week02/scripts/bom.js
+++ week02/scripts/bom.js
@@ -1,10 +1,34 @@
-const input= document.querySelector("#favchap");
-const button= document.querySelector("button");
-const list = document.querySelector("_______");
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list"); 
 
-const li = document.createrElement("li");
-const deleteButton= document.createElement("button");
-li.textContent = input.value;
-deleteButton.textContent= "❌";
-li.append(deleteButton);
-list.append(li);
+button.addEventListener("click", function() {
+  if (input.value.trim() !== '') {
+    const myItem = input.value;
+    input.value = ''; 
+
+    // Crear nuevos elementos
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listText.textContent = myItem;
+    listBtn.textContent = 'Eliminar';
+
+    
+    listItem.appendChild(listText);
+    listItem.appendChild(listBtn);
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+
+
+    input.focus();
+  }
+});
+
+
+
+      
