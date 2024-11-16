2dcddfeeef5ad615af107a66e221b0c21652159c
dmquiroga11 <qui24003@byui.edu>
2024-11-16 08:14:18 -0500

    media temples

---
+++ scripts/temples.js
@@ -0,0 +1,12 @@
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = document.lastModified;
+
+const currentTime = document.querySelector("#currentTime");
+
+function updateTime() {
+    const now = new Date();
+    currentTime.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateTime, 1000);
+updateTime();
