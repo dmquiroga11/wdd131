3e656025955d2d52a7d75b65ec48befef6b4e7cf
dmquiroga11 <qui24003@byui.edu>
2024-11-16 11:37:42 -0500

    already 3

--- scripts/temples.js
+++ scripts/temples.js
@@ -10,3 +10,16 @@ function updateTime() {
 
 setInterval(updateTime, 1000);
 updateTime();
+
+const nav = document.querySelector("#nav");
+const open = document.querySelector("#open");
+const close = document.querySelector("#close");
+
+open.addEventListener("click",()=>{
+    nav.classList.add("visible");
+})
+
+close.addEventListener("click", () => {
+    nav.classList.remove("visible");
+})
+
