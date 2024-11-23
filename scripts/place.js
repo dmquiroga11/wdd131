7b662426cd68f1e57066e633fa7576428d982ddc
dmquiroga11 <qui24003@byui.edu>
2024-11-23 12:49:49 -0500

    js commit

--- scripts/place.js
+++ scripts/place.js
@@ -1,3 +1,21 @@
+
+const temperature =27;
+const windSpeed = 14;
+
+
+function windChill(temperature, windSpeed) {
+    const windChill = 13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16);
+    return windChill;
+}
+function updateWindChill() {
+     const windChillValue = windChill(temperature, windSpeed); 
+     const windChillElement = document.getElementById("windChillResult"); 
+     windChillElement.innerText = `${windChillValue.toFixed(2)}°C`; } 
+
+document.addEventListener("DOMContentLoaded", updateWindChill);
+
+
+
 const lastModified = document.querySelector("#lastModified");
 lastModified.textContent = document.lastModified;
 
