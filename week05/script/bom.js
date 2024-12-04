c8357570fce6f1b1297dfa98f336e3e5b93102d1
dmquiroga11 <qui24003@byui.edu>
2024-12-04 17:20:31 -0500

    practice week05

---
+++ week05/script/bom.js
@@ -0,0 +1,30 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector("button");
+const list = document.querySelector("#list"); 
+
+button.addEventListener("click", function() {
+  if (input.value.trim() !== '') {
+    const myItem = input.value;
+    input.value = ''; 
+
+    // Crear nuevos elementos
+    const listItem = document.createElement("li");
+    const listText = document.createElement("span");
+    const listBtn = document.createElement("button");
+
+    listText.textContent = myItem;
+    listBtn.textContent = 'Eliminar';
+
+    
+    listItem.appendChild(listText);
+    listItem.appendChild(listBtn);
+    list.appendChild(listItem);
+
+    listBtn.addEventListener('click', () => {
+      list.removeChild(listItem);
+    });
+
+
+    input.focus();
+  }
+});
