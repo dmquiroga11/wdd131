9e1af084ca679268bd5dcb54028407dd87ebb893
dmquiroga11 <qui24003@byui.edu>
2024-12-06 18:10:59 -0500

    form page

---
+++ scripts/form.js
@@ -0,0 +1,45 @@
+const lastModified = document.querySelector("#lastModified");
+lastModified.textContent = document.lastModified;
+
+const currentTime = document.querySelector("#currentTime");
+
+function updateTime() {
+    const now = new Date();
+    currentTime.textContent = now.toLocaleTimeString();
+}
+
+setInterval(updateTime, 1000);
+updateTime();
+
+const products = [
+    {
+      id: "fc-1888",
+      name: "flux capacitor",
+      averagerating: 4.5
+    },
+    {
+      id: "fc-2050",
+      name: "power laces",
+      averagerating: 4.7
+    },
+    {
+      id: "fs-1987",
+      name: "time circuits",
+      averagerating: 3.5
+    },
+    {
+      id: "ac-2000",
+      name: "low voltage reactor",
+      averagerating: 3.9
+    },
+    {
+      id: "jj-1969",
+      name: "warp equalizer",
+      averagerating: 5.0
+    }
+  ]; 
+
+    const selectElement = document.getElementById('product');
+    products.forEach(product => { const option = document.createElement('option');
+    option.value = product.name; option.textContent = product.name;
+    selectElement.appendChild(option); });
